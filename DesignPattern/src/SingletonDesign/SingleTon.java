66a80b2cfa5756663c70794610ef61a207ab5b2f
monaa <Mona.Agrawal@amdocs.com>
2024-04-03 20:50:51 +0530

    Break the singleTon pattern using Java reflection API

--- DesignPattern/src/SingletonDesign/SingleTon.java
+++ DesignPattern/src/SingletonDesign/SingleTon.java
@@ -1,8 +1,10 @@
 package SingletonDesign;
 
+import java.lang.reflect.Constructor;
+
 public class SingleTon {
 
-	public static void main(String str[])
+	public static void main(String str[]) throws Exception
 	{
 		TesterDesign test1=TesterDesign.getInstance();
 		TesterDesign test2=TesterDesign.getInstance();
@@ -20,5 +22,15 @@ public class SingleTon {
 		System.out.println(test4.hashCode());
 		System.out.println(test5.hashCode());
 		System.out.println(test6.hashCode());
+		
+		//How to break singleton class
+		//Get the constructor and store it in constructor class
+		Constructor<TesterDesign> con= TesterDesign.class.getDeclaredConstructor();
+		//change the accessibility as we are getting exception
+		con.setAccessible(true);
+		//get new instance using con
+		TesterDesign tester= con.newInstance();
+		
+		System.out.println(tester.hashCode());
 	}
 }
